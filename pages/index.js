df0b4860772d787a2da9d1f646809d6023f06cf9
a22215987321-byte <a22215987321@gmail.com>
2026-06-04 02:43:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <h1>🎉 部署成功！</h1>
-      <p>我的美食小站 v2 已上線～</p>
+      <p>我的美食小站 v2 已經上線了。</p>
     </div>
   );
 }
